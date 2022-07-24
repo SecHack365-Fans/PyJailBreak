778a2ecfa415d0e053c8b308a98127548fbe3d0c
xryuseix <ryusei143.shootingstar@gmail.com>
2022-07-24 20:45:39 +0900

    feat: トップページにPyJailBreakの説明を追加

--- webconsole/pages/index.tsx
+++ webconsole/pages/index.tsx
@@ -1,10 +1,28 @@
+/** @format */
+
 import type { NextPage } from "next";
 import Layout from "../components/Layout";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      <img src="/PyJailBreak/PyJailBreak.png" alt="title logo" />
+      <div className={styles.description_section}>
+        <div className={styles.logo}>
+          <img src="/PyJailBreak/PyJailBreak.png" alt="title logo" />
+        </div>
+        <div className={styles.description}>
+          <img
+            src="/PyJailBreak/logo_horizontal_cleanness.png"
+            alt="PyJailBreak"
+            className={styles.logo_hirizontal}
+          />{" "}
+          is a <br />
+          <b>Black box automated vulnerability scanning tool</b>
+          <br />
+          for Python socket servers.
+        </div>
+      </div>
     </Layout>
   );
 };
