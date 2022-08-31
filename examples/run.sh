ee3da39e8727417569a8008e3dce8feac211a9b4
Satoki <t.satoki111@gmail.com>
2022-09-01 01:31:40 +0900

    fix examples

--- examples/run.sh
+++ examples/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# examples_01:4441
+# example_01:4441
 socat TCP-L:4441,fork,reuseaddr EXEC:"python3 ./example_01/app.py",pty,stderr,setsid,sane,raw,echo=0 &
 
-# examples_02:4442
+# example_02:4442
 socat TCP-L:4442,fork,reuseaddr EXEC:"python3 ./example_02/app.py",pty,stderr,setsid,sane,raw,echo=0 &
 
-# examples_03:4443
+# example_03:4443
 socat TCP-L:4443,fork,reuseaddr EXEC:"python3 ./example_03/app.py",pty,stderr,setsid,sane,raw,echo=0 &
 
-# examples_04:4444
+# example_04:4444
 socat TCP-L:4444,fork,reuseaddr EXEC:"python3 ./example_04/app.py",pty,stderr,setsid,sane,raw,echo=0
