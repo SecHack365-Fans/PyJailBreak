e8325239c695850db101196867e7a793024bc428
xryuseix <ryusei143.shootingstar@gmail.com>
2022-09-05 00:46:19 +0900

    feat: set OGP

--- website/components/Layout/Header.tsx
+++ website/components/Layout/Header.tsx
@@ -9,21 +9,26 @@ const Header: React.FC<Props> = ({ title, description }: Props) => {
   const defaultTitle = "PyJailBreak";
   const defaultDescription =
     "Black box automated vulnerability scanning tool for Python socket servers";
+  const perfectTitle = title ? title : defaultTitle
+  const perfectDescription = description ? description : defaultTitle
   return (
     <Head>
-      <title>{title ? title : defaultTitle}</title>
+      <title>{title}</title>
       <meta
         name="description"
         content={description ? description : defaultDescription}
       />
       <link rel="icon" href="/PyJailBreak/favicon.ico" />
-      <meta name="og:title" content={title} />
+      <meta name="og:url" content="https://fans.sechack365.com/PyJailBreak/" />
+      <meta name="og:title" content={perfectTitle} />
+      <meta name="og:description" content={perfectDescription} />
       <meta property="og:type" content="website" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta
         property="og:image"
-        content="https://pbs.twimg.com/card_img/1551139193656844288/N9ApjUm4?format=jpg&amp;name=orig"
+        content="https://raw.githubusercontent.com/SecHack365-Fans/PyJailBreak/main/assets/logo_readme_title.png"
       />
+      <meta name="twitter:card" content="summary" />
     </Head>
   );
 };
